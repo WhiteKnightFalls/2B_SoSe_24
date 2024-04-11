4fd37bb6784994a5a5a888081b17bf5a10128e64
WhiteKnightFalls <75867231+WhiteKnightFalls@users.noreply.github.com>
2024-04-11 13:04:32 +0200

    PlayerControl, collectible, interactable
    Kamera mit Cinemachine verfeinert,
    Player kann collectibles aufsammeln und interagieren

--- Assets/Codes/PlayerController.cs
+++ Assets/Codes/PlayerController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,21 +10,28 @@ public class PlayerController : MonoBehaviour
 
     private InputAction _move;
     private InputAction _look;
+    private InputAction _interact;
 
     private float _cameraXRotation;
+
+    private Vector3 _velocity;
+    //-------------------------------------------------------
     
     public PlayerInput PlayerInput;
     public CharacterController Controller;
     public Transform Camera;
     public float Speed = 10f;
-    
+
+    public float Gravity = -9.8f; // m/s/s für Schwerkraft berechnen
+
+    public Interactable CurrentInteractable;
     
     // Start is called before the first frame update
     void Start()
     {   //in Visuel Scripting gibt es ein drop down, mit action finden und variable speichern
         _move = PlayerInput.actions.FindAction("Move");
-        
         _look = PlayerInput.actions.FindAction("Look");
+        _interact = PlayerInput.actions.FindAction("Interact");
 
         _cameraXRotation = Camera.rotation.eulerAngles.x;
     }
@@ -41,8 +50,20 @@ public class PlayerController : MonoBehaviour
     
         //forwärstrichtung
         Vector3 moveAmount = transform.forward * moveInput.z + transform.right * moveInput.x;
-        Controller.Move(moveInput * Speed * Time.deltaTime);
+        moveAmount.y += Gravity * Time.deltaTime;
         
+        _velocity.x = moveAmount.x;
+        _velocity.y += Gravity * Time.deltaTime;
+        _velocity.z = moveAmount.z;
+        
+        //OnCollisionEnter
+        if(Controller.Move(_velocity * Speed * Time.deltaTime) == CollisionFlags.Below)
+        {
+            _velocity.y = 0;
+        }
+        
+
+     
         
         //holen ein Vector2 aus dem _look 
         Vector2 lookInput = _look.ReadValue<Vector2>();
@@ -72,5 +93,54 @@ public class PlayerController : MonoBehaviour
         cameraPosition.y = y;
         Camera.position = cameraPosition;
         */
+        /*
+        //hold
+        _interact.IsPressed();
+        //pressed
+        _interact.WasPerformedThisFrame();
+        //released
+        _interact.WasReleasedThisFrame();
+        */
+
+        if (_interact.WasPressedThisFrame() && CurrentInteractable != null)
+        {
+            CurrentInteractable.Interact();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //option 1. tag
+      /*  if(other.CompareTag("coin"))
+        {
+            
+        }
+        if(other.CompareTag("coin"))
+        {
+            
+        }
+       */
+        //option 2. component
+        Collectable collectable = other.GetComponent<Collectable>();
+        //Ist collectable nicht null, kann ich den Collect aufrufen
+        if (collectable != null)
+        {
+            collectable.Collect();
+        }
+         //interactable
+         Interactable inter = other.GetComponent<Interactable>();
+         if (inter != null)
+         {
+             CurrentInteractable = inter;
+         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Interactable inter = other.GetComponent<Interactable>();
+        if (inter != null)
+        {
+            CurrentInteractable = null;
+        }
     }
 }
